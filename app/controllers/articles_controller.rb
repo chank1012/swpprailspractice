ac069cb54f3765ab0580ead93ff00b351de4a22a
prox <chank1012@naver.com>
2015-04-06 20:36:20 +0900

    article counter added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+  def count
+    num_of_articles = Article.all.count
+    render :json => { count: num_of_articles }
+  end
+
   def edit
     @article = Article.find(params[:id])
   end
